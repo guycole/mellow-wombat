2bb4870f65bb5597f09bcc88aa54bcab49ebb7be
Guy Cole <guycole@gmail.com>
2022-02-17 23:12:21 -0800

    more django

--- django/mellow/wombat/models.py
+++ django/mellow/wombat/models.py
@@ -1,3 +1,23 @@
+#
+# Title:models.py
+# Description:
+# Development Environment:OS X 10.15.7/Python 3.9.9/Django 4.0.2
+# Author:Guy Cole (guycole at gmail dot com)
+#
 from django.db import models
 
-# Create your models here.
+class GeoLoc(models.Model):
+    id = models.BigAutoField(primary_key = True)
+    time_stamp = models.BigIntegerField()
+    latitude = models.CharField(max_length=16)
+    longitude = models.CharField(max_length=16)
+
+    def __repr__(self):
+        return f"{self.id} {self.time_stamp} {self.latitude} {self.longitude}"
+
+    def __str__(self):
+        return f"{self.id} {self.time_stamp} {self.latitude} {self.longitude}"
+
+# ;;; Local Variables: ***
+# ;;; mode:python ***
+# ;;; End: ***
