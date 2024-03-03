8ef9397f1bd995bc4ed296614c3b165d4a21c87f
Mongo Hax <guycole@gmail.com>
2024-03-03 23:02:54 +0000

    django clean up

--- src/django/wombat/hyena/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import AdsbExchange, LoadLog, Observation 
-
-admin.site.register(AdsbExchange)
-admin.site.register(LoadLog)
-admin.site.register(Observation)
