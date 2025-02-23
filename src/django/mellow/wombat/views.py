8b61e0c50c91302f8803eedc24f661ac17e5bd17
Guy Cole <guycole@gmail.com>
2025-02-23 01:26:50 +0000

    broken gunicorn

--- src/django/mellow/wombat/views.py
+++ src/django/mellow/wombat/views.py
@@ -5,18 +5,18 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Heeler
+#from .models import Heeler
 
 def index(request):
     return HttpResponse("mellow wombat crate 1")
 
-class HeelerView(generic.ListView):
-    template_name = "wombat/heeler.html"
-
-    context_object_name = "observation_list"
-
-    def get_queryset(self):
-        return Heeler.objects.all()
+#class HeelerView(generic.ListView):
+#    template_name = "wombat/heeler.html"
+#
+#    context_object_name = "observation_list"
+#
+#    def get_queryset(self):
+#        return Heeler.objects.all()
 
 #    @csrf_exempt
 #    def dispatch(self, request, *args, **kwargs):
