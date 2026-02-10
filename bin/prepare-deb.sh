4756b9c8720ae40de1b1e7a4469f00af5cf2f0b1
Guy Cole <guycole@gmail.com>
2026-02-09 18:23:41 -0800

    package test

---
+++ bin/prepare-deb.sh
@@ -0,0 +1,7 @@
+cd /home/gsc/Documents/github/mellow-wombat
+dpkg-deb --build mellow_wombat_package_1.0_all
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+gpg --default-key "guycole@gmail.com" -abs -o - Release > Release.gpg
+gpg --default-key "guycole@gmail.com" --clearsign -o - Release > InRelease
