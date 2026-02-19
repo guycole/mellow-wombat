ae9c215c73ee872c7518846283d63efca663cb9e
Guy Cole <guycole@gmail.com>
2026-02-18 19:18:19 -0800

    more documentation

--- bin/prepare-deb.sh
+++ bin/prepare-deb.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#
+# Title: prepare-deb.sh
+# Description: prepare mellow wombat debian package
+# Development Environment: ubuntu 22.04.5 LTS
+# Author: Guy Cole (guycole at gmail dot com)
+#
 cd /home/gsc/Documents/github/mellow-wombat
 dpkg-deb --build mellow_wombat_package_1.0_all
 dpkg-scanpackages --multiversion . > Packages
