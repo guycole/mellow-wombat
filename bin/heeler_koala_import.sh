859cab8a1a71323297642ed1f1ff584fea845869
Guy Cole <guycole@gmail.com>
2026-04-26 07:02:02 +0000

    import script

---
+++ bin/heeler_koala_import.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Title: heeler_koala_import.sh
+# Description: import koala files 
+# Development Environment: Ubuntu 22.04.05 LTS
+# Author: Guy Cole (guycole at gmail dot com)
+#
+PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
+#
+#
+SOURCE_DIR="/var/wombat/heeler/koala"
+KOALA_IMPORT="/home/wombat/Documents/github/mellow-koala/bin/import_heeler"
+#
+
+#
