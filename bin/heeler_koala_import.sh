787c80c1d41a39e65a922a1025eb4b14032ce626
Guy Cole <guycole@gmail.com>
2026-04-26 00:05:36 -0700

    updated import script

--- bin/heeler_koala_import.sh
+++ bin/heeler_koala_import.sh
@@ -11,5 +11,14 @@ PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 SOURCE_DIR="/var/wombat/heeler/koala"
 KOALA_IMPORT="/home/wombat/Documents/github/mellow-koala/bin/import_heeler"
 #
+for FILE in "$SOURCE_DIR"/*; do
+  [ -f "$FILE" ] || continue
 
+  "$KOALA_IMPORT" "$FILE"
+  if [ $? -eq 0 ]; then
+    rm "$FILE"
+  else
+    echo "ERROR: import failed for $FILE" >&2
+  fi
+done
 #
