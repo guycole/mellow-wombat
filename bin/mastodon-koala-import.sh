8cfef2bf618b0d2e6409ea83ad12fad370f6e30e
Guy Cole <guycole@gmail.com>
2026-07-18 09:37:25 +0000

    mastodon koala

---
+++ bin/mastodon-koala-import.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Title: mastodon-koala-import.sh
+# Description: import koala files 
+# Development Environment: Ubuntu 22.04.05 LTS
+# Author: Guy Cole (guycole at gmail dot com)
+#
+RBENV_ROOT="${RBENV_ROOT:-/home/wombat/.rbenv}"
+if [ ! -d "$RBENV_ROOT" ] && [ -d "$HOME/.rbenv" ]; then
+  RBENV_ROOT="$HOME/.rbenv"
+fi
+PATH="$RBENV_ROOT/shims:$RBENV_ROOT/bin:/bin:/usr/bin:/etc:/usr/local/bin:/snap/bin${PATH:+:$PATH}"; export PATH
+export RBENV_ROOT
+#
+#
+SOURCE_DIR="/var/wombat/mastodon/koala"
+KOALA_IMPORT="/home/wombat/github/mellow-koala/bin/import_mastodon"
+#
+for FILE in "$SOURCE_DIR"/*; do
+  [ -f "$FILE" ] || continue
+
+  "$KOALA_IMPORT" "$FILE"
+  if [ $? -eq 0 ]; then
+    rm "$FILE"
+  else
+    echo "ERROR: import failed for $FILE" >&2
+  fi
+done
+#
--- bin/mastodon-validate.sh
+++ bin/mastodon-validate.sh
@@ -9,9 +9,11 @@ PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
 echo "start validate"
 #
-docker rm mastodon;docker run -v /var/wombat:/mnt/wombat --name mastodon mastodon:latest
+docker rm mastodon-validate;docker run -v /var/wombat:/mnt/wombat --name mastodon-validate mastodon:latest
 #
-#/home/wombat/github/mellow-wombat/bin/heeler-koala-import.sh
+docker rm mastodon-koala;docker run -e stuntbox=koala -v /var/wombat:/mnt/wombat --name mastodon-koala mastodon:latest
+#
+#$HOME/github/mellow-wombat/bin/mastodon-koala-import.sh
 #
 echo "end validate"
 #
