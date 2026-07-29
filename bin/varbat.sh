2cb90802854806b6fb9b09c57b6ae8b514c0a283
Guy Cole <guycole@gmail.com>
2026-07-29 03:48:08 +0000

    make varbat

---
+++ bin/varbat.sh
@@ -0,0 +1,8 @@
+mkdir admin capybara failure fresh heeler hyena manatee mastodon slug
+mkdir fresh/capybara fresh/heeler fresh/hyena fresh/manatee fresh/mastodon fresh/slug
+mkdir capybara/archive capybara/export capybara/koala capybara/success
+mkdir heeler/archive heeler/export heeler/koala heeler/success
+mkdir hyena/archive hyena/export hyena/koala hyena/success
+mkdir manatee/archive manatee/export manatee/koala manatee/success
+mkdir mastodon/archive mastodon/export mmastodon/koala mastodon/success
+mkdir slug/archive slug/export slug/koala slug/success
